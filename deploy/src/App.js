0693e25fbb4127d453492a8825b55391d77ecb4a
Scott Grobe <gitgrobe@gmail.com>
2020-02-23 08:45:31 -0500

    edit text

--- deploy/src/App.js
+++ deploy/src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          deploying a web app
+      deploying a web app.
         </a>
       </header>
     </div>
